cbe0c8762b5ec113f68746f972cdd10853a7b402
Veeagony <159520361+Veeagony@users.noreply.github.com>
2025-04-10 12:29:05 +0800

    update

--- src/DisciplinaryOffice/Dashboard/Dashboard.js
+++ src/DisciplinaryOffice/Dashboard/Dashboard.js
@@ -2,14 +2,9 @@ import React, { useState } from "react";
 import "./Dashboard.css";
 import { FaBalanceScale, FaUserGraduate, FaCalendarAlt } from "react-icons/fa";
 
-
 function Dashboard() {
-
-
   return (
     <div className="d-flex">
-  
-
       {/* Main Content */}
       <div className="main-content flex-grow-1 p-4">
         <div className="dashboard-header d-flex justify-content-between align-items-center">
@@ -23,19 +18,28 @@ function Dashboard() {
         <div className="row mt-4 g-4">
           <div className="col-md-4">
             <div className="card bg-danger text-white">
-              <p><FaBalanceScale /> Violation</p>
+              <div className="card-icon">
+                <FaBalanceScale />
+              </div>
+              <h4>Violation</h4>
               <h3>0</h3>
             </div>
           </div>
           <div className="col-md-4">
             <div className="card bg-success text-white">
-              <p><FaUserGraduate /> Incident Reports</p>
+              <div className="card-icon">
+                <FaUserGraduate />
+              </div>
+              <h4>Incident Reports</h4>
               <h3>0</h3>
             </div>
           </div>
           <div className="col-md-4">
             <div className="card bg-primary text-white">
-              <p><FaCalendarAlt /> Appointment</p>
+              <div className="card-icon">
+                <FaCalendarAlt />
+              </div>
+              <h4>Appointment</h4>
               <h3>0</h3>
             </div>
           </div>
